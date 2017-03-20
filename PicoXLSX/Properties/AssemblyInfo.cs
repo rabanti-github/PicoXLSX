04586933d6142c7adabce18989d80f2df08bbd69
Raphael Stoeckli <github@rabanti.ch>
2017-03-20 01:29:24 +0100

    Update v1.5.4 (see change log)

--- PicoXLSX/Properties/AssemblyInfo.cs
+++ PicoXLSX/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("PicoXLSX")]
-[assembly: AssemblyCopyright ("Copyright Raphael Stoeckli © 2016")]
+[assembly: AssemblyCopyright("Copyright Raphael Stoeckli © 2017")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("1.5.3")]
-[assembly: AssemblyFileVersion ("1.5.3")]
+[assembly: AssemblyVersion("1.5.4")]
+[assembly: AssemblyFileVersion("1.5.4")]
